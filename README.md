cc4993d443128a87ed7a56cc264b42bae5fae772
Daniel Fanica <contact@danielfanica.com>
2023-03-17 01:55:18 +0000

    Switch to use ddcutil for monitor controls via API

--- README.md
+++ README.md
@@ -1,2 +1,99 @@
 # MagicMirror-WebApp
 Display Controls &amp; API
+
+
+# Raspberry Pi GPIO Control with Flask
+
+This is a Flask web application designed to control various features of a Raspberry Pi using GPIO pins. The application serves two purposes: first, it provides a web-based UI for controlling the Raspberry Pi GPIO pins, and second, it provides a REST API for controlling the Raspberry Pi from external programs.
+
+## Getting Started
+
+To run the application, you will need to install Flask and ddcutil. Once you have installed these packages, you can start the application by running `python app.py`.
+
+## Application Structure
+
+The application is structured as follows:
+
+* `app.py`: This is the main application file. It contains the Flask routes and the API endpoints.
+* `templates/`: This directory contains the HTML templates used by the application.
+* `static/`: This directory contains static files used by the application, such as CSS stylesheets and JavaScript files.
+
+## Flask Routes
+
+The following Flask routes are defined in `app.py`:
+
+* `/`: The root route, which displays the main UI for the application.
+* `/q`: A route that presses the power button.
+* `/w`: A route that presses the menu button.
+* `/e`: A route that presses the left button.
+* `/aim_assist`: A route that presses the aim assist button.
+* `/r`: A route that presses the right button.
+* `/view_mode`: A route that presses the view mode button.
+* `/t`: A route that presses the auto button.
+* `/input_select`: A route that presses the input select button.
+* `/service-menu`: A route that enters the service menu.
+* `/api/audio-output`: An API endpoint that returns the current status of the audio output.
+* `/api/display-state`: An API endpoint that returns the current state of the display (on or off).
+* `/api/display-on`: An API endpoint that turns on the display.
+* `/api/display-off`: An API endpoint that turns off the display.
+* `/api/display-brightness`: An API endpoint that returns the current brightness level of the display.
+* `/api/display-brightness-low`: An API endpoint that sets the brightness of the display to low.
+* `/api/display-brightness-high`: An API endpoint that sets the brightness of the display to high.
+
+## REST API
+
+The following REST API endpoints are defined in `app.py`:
+
+* `/api/audio-output`: An API endpoint that returns the current status of the audio output.
+* `/api/display-state`: An API endpoint that returns the current state of the display (on or off).
+* `/api/display-on`: An API endpoint that turns on the display.
+* `/api/display-off`: An API endpoint that turns off the display.
+* `/api/display-brightness`: An API endpoint that returns the current brightness level of the display.
+* `/api/display-brightness-low`: An API endpoint that sets the brightness of the display to low.
+* `/api/display-brightness-high`: An API endpoint that sets the brightness of the display to high.
+
+## GPIO Pins
+
+The following GPIO pins are used by the application:
+
+* Pin 1: Used for the menu button.
+* Pin 7: Used for the right button and the view mode button.
+* Pin 8: Used for the left button and the aim assist button.
+* Pin 12: Used for the power button.
+* Pin 25: Used for the input select button.
+
+## Display Control
+
+The display is controlled using ddcutil. The following commands are used:
+
+* `ddcutil detect`: Detects the connected display.
+* `ddcutil setvcp d6 1`: Turns on the display.
+* `ddcutil setvcp d6 5`: Turns off the display.
+* `ddcutil getvcp 10`: Gets the current brightness level of the display.
+* `ddcutil setvcp 10 10`: Sets the brightness of the display to low.
+* `ddcutil setvcp 10 100`: Sets the brightness of the display to high.
+
+## Audio Output Control
+
+The audio output is controlled using the ALSA API. The following commands are used:
+
+* `cat /proc/asound/card*/pcm*p/sub*/hw_params`: Gets the current audio output state.
+* `grep -q "access"`: Checks if the audio output is enabled.
+
+## Service Menu
+
+The service menu is accessed by pressing the menu and power buttons simultaneously. The following commands are used:
+
+* `raspi-gpio set 1 op dl`: Sets GPIO pin 1 to low.
+* `raspi-gpio set 12 op dl`: Sets GPIO pin 12 to low.
+* `raspi-gpio set 12 op dh`: Sets GPIO pin 12 to high.
+* `raspi-gpio set 1 op dh`: Sets GPIO pin 1 to high.
+
+## Development
+
+To run the application in development mode, uncomment the `app.run(debug=True, host="0.0.0.0", port="5000")` line in `app.py`.
+
+## Production
+
+To run the application in production mode, uncomment the `app.run(host='0.0.0.0', port=80)` line in `app.py`. Note that you will need to run the application as root to use port 80.
+
--- webapp.py
+++ webapp.py
@@ -3,107 +3,162 @@ import os
 import subprocess
 
 app = Flask(__name__, instance_relative_config=True)
-print("Done")
 
-## ROOT Route
+# GPIO Commands
+pwr_press = "raspi-gpio set 12 op dl && sleep 0.1 && raspi-gpio set 12 op dh"
+menu_press = "raspi-gpio set 1 op dl && sleep 0.1 && raspi-gpio set 1 op dh"
+left_press = "raspi-gpio set 8 op dl && sleep 0.1 && raspi-gpio set 8 op dh"
+aim_assist_press = "raspi-gpio set 8 op dl && sleep 0.1 && raspi-gpio set 8 op dh"
+right_press = "raspi-gpio set 7 op dl && sleep 0.1 && raspi-gpio set 7 op dh"
+view_mode_press = "raspi-gpio set 7 op dl && sleep 0.1 && raspi-gpio set 7 op dh"
+auto_press = "raspi-gpio set 25 op dl && sleep 0.1 && raspi-gpio set 25 op dh"
+input_select_press = "raspi-gpio set 25 op dl && sleep 0.1 && raspi-gpio set 25 op dh"
+combo_press = "raspi-gpio set 1 op dl && sleep 0.1 && raspi-gpio set 12 op dl && sleep 0.1 && raspi-gpio set 12 op dh && sleep 1 && raspi-gpio set 1 op dh"
 
+# Helper functions
+def getDisplayState():
+    state = subprocess.check_output(  
+        'ddcutil getvcp d6 | grep -Po "(?<=sl=)(0x[0-9a-f]+)"',
+        shell=True
+    )
+    if state.strip() == b'0x01':
+        return 'on'
+    elif state.strip() == b'0x05':
+        return 'off'
+    else:
+        return 'unknown'
+
+def getAudioOutputState():
+    state = subprocess.check_output(
+        'if cat /proc/asound/card*/pcm*p/sub*/hw_params | grep -q "access"; then echo 1; else echo 0; fi',
+        shell=True
+    )
+    if state.strip() == b'1':
+        return 'on'
+    else:
+        return 'off'
+
+def setDisplayBrightness(brightness_level):
+    state = getDisplayState()
+    # Turn the display on if off
+    if state == 'off':
+        os.system('ddcutil setvcp d6 1')
+    os.system('ddcutil setvcp 10 {0}'.format(brightness_level))
+    return {
+        "service": "display_brightness_level", 
+        "online": state,
+        "value": '{0}'.format(brightness_level)
+    }
+
+# Root Route
 @app.route('/')
 def index():
     # Grab "menu_on" get param
     menu_on = request.args.get('menu_on')
     return render_template('webpage.html', menu_on=menu_on)
 
-## GPIO Monitor Controls Routes
-
+# GPIO Monitor Controls Routes
 @app.route('/q')
 def pwrPress():
-    os.system ("raspi-gpio set 12 op dl && sleep 0.1 && raspi-gpio set 12 op dh")
+    os.system(pwr_press)
     return redirect('/')
 
 @app.route('/w')
 def menuPress():
-    os.system ("raspi-gpio set 1 op dl && sleep 0.1 && raspi-gpio set 1 op dh")
+    os.system(menu_press)
     return redirect('/?menu_on=1')
 
 @app.route('/e')
 def leftPress():
-    os.system ("raspi-gpio set 8 op dl && sleep 0.1 && raspi-gpio set 8 op dh")
+    os.system(left_press)
     return redirect('/?menu_on=1')
+
 @app.route('/aim_assist')
 def aimAssistPress():
-    os.system ("raspi-gpio set 8 op dl && sleep 0.1 && raspi-gpio set 8 op dh")
+    os.system(aim_assist_press)
     return redirect('/')
 
 @app.route('/r')
 def rightPress():
-    os.system ("raspi-gpio set 7 op dl && sleep 0.1 && raspi-gpio set 7 op dh")
+    os.system(right_press)
     return redirect('/?menu_on=1')
+
 @app.route('/view_mode')
 def viewModePress():
-    os.system ("raspi-gpio set 7 op dl && sleep 0.1 && raspi-gpio set 7 op dh")
+    os.system(view_mode_press)
     return redirect('/')
 
 @app.route('/t')
 def autoPress():
-    os.system ("raspi-gpio set 25 op dl && sleep 0.1 && raspi-gpio set 25 op dh")
+    os.system(auto_press)
     return redirect('/?menu_on=1')
+
 @app.route('/input_select')
 def inputSelectPress():
-    os.system ("raspi-gpio set 25 op dl && sleep 0.1 && raspi-gpio set 25 op dh")
+    os.system(input_select_press)
     return redirect('/')
 
 @app.route('/service-menu')
 def comboPress():
-    os.system("raspi-gpio set 1 op dl && sleep 0.1 && raspi-gpio set 12 op dl && sleep 0.1 && raspi-gpio set 12 op dh && sleep 1 && raspi-gpio set 1 op dh")
+    os.system(combo_press)
     return redirect('/')
 
-## API Routes
-
+# API Routes
 @app.route('/api/audio-output')
 def statusAudioOuput():
-    state = subprocess.check_output(
-        'if cat /proc/asound/card*/pcm*p/sub*/hw_params | grep -q "access"; then echo 1; else echo 0; fi',
-        shell=True
-    )
     return {
         "service": "audio_output",
-        "status": state.strip()
+        "status": getAudioOutputState()
     }
 
 @app.route('/api/display-state')
 def statusDisplay():
-    state = subprocess.check_output(
-        'if tvservice --status | grep -q "HDMI CEA"; then echo 1; else echo 0; fi',
-        shell=True
-    )
     return {
         "service": "display_state",
-        "power": state.strip()
+        "power": getDisplayState()
     }
 
 @app.route('/api/display-on')
 def switchDisplayOn():
-    state = subprocess.check_output(
-        'if tvservice --status | grep -q "HDMI CEA"; then echo 1; else echo 0; fi',
-        shell=True
-    )
+    state = getDisplayState()
     # Turn the display on if off
-    if state.strip() == b'0': os.system('tvservice --preferred && sudo chvt 6 && sudo chvt 7')
+    if state == 'off':
+        os.system(combo_press)
     return statusDisplay()
 
 @app.route('/api/display-off')
 def switchDisplayOff():
-    state = subprocess.check_output(
-        'if tvservice --status | grep -q "HDMI CEA"; then echo 1; else echo 0; fi',
-        shell=True
-    )
+    state = getDisplayState()
     # Turn the display off if on
-    if state.strip() == b'1': os.system('tvservice -o')
+    if state == 'on':
+        os.system('ddcutil setvcp d6 5')
     return statusDisplay()
 
-if __name__=="__main__":
+@app.route('/api/display-brightness')
+def getDisplayBrightness():
+    brightness = '0'
+    # Check online state of display
+    state = getDisplayState()
+    # Set brighness to current level only if online
+    if state == 'on':
+        brightness = subprocess.check_output(
+            'ddcutil getvcp 10 | grep -Po "(?<=\\s)([0-9]+)(?=\,)"',
+            shell=True
+        )
+    return {
+        "service": "display_brightness_level", 
+        "online": state,
+        "value": brightness.strip()
+    }
+
+@app.route('/api/display-brightness/<int:brightness_level>')
+def setDisplayBrightnessAPI(brightness_level):
+    return setDisplayBrightness(brightness_level)
+
+if __name__ == '__main__':
     print("Start")
     # Debug/Development
     # app.run(debug=True, host="0.0.0.0", port="5000")
     # Production
     app.run(host='0.0.0.0', port=80)
+
